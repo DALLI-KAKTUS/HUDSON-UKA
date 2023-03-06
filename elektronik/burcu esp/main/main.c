bad1ab9725a2c1a5ac86b6c22e8cd2f1418d629d
BurcuBulut00 <116095710+BurcuBulut00@users.noreply.github.com>
2023-03-06 10:45:36 +0300

    Add files via upload

--- elektronik/burcu esp/main/main.c
+++ elektronik/burcu esp/main/main.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,7 +8,7 @@
 #include "ssd1306.h"
 #include "font8x8_basic.h"
 
-//sda ve cdl pinlerinin tanimlanması
+//sda ve scl pinlerini tanimlandi.
 #define sda_gpio 22
 #define scl_gpio 23
 
@@ -17,21 +16,22 @@
 
 void app_main(void)
 {
-	 SSD1306_t dev;
-	 int center, top, bottom;
-	 char lineChar[20];
+	SSD1306_t dev; //device tanimlama
+//Islemlerde dev tanimlandi.
+	i2c_master_init(&dev, sda_gpio, scl_gpio, 19);//ssd1306'nin hangi esp pinlerine bagli oldugunu yazdik. 19 pin icin tanimlandi.
 
-	 i2c_master_init(&dev, sda_gpio, scl_gpio, 19);
+	ssd1306_init(&dev, 128, 64);//Ekran boyutlari tanimlandi.
 
-	 ssd1306_init(&dev, 128, 64);
+	ssd1306_clear_screen(&dev, false);//Ekran temizlemek icin :(false).
+	ssd1306_contrast(&dev, 0xff);//Siyah ve beyaz renklerin daha net gorunmesi icin contrast ayari: 0xff 
 
-	 ssd1306_clear_screen(&dev, false);
-	 ssd1306_contrast(&dev, 0xff);
+  ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);//(x3 yazi boyutu icn) 0 ilk satiri tanimlar, 5 ekrana yazdirilacak karakter sayisini tanimlar. (false):siyah ekran uzerine beyaz yazi yazmak icin yazildi.
+  vTaskDelay(3000 / portTICK_PERIOD_MS);//3 saniye bekleme
+  ssd1306_clear_screen(&dev, false);//Ekrana yazilan yazilarin silinmesi
 
-	 ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);
-	 vTaskDelay(3000 / portTICK_PERIOD_MS);
-	 ssd1306_clear_screen(&dev, false);
-	 ssd1306_display_text(&dev, 1, "doayı seviom", 12, false);
-	 vTaskDelay(3000 / portTICK_PERIOD_MS);
-	 ssd1306_display_text(&dev, 2, "berke", 5, true);
+  ssd1306_display_text(&dev, 1, "ssd", 3, false);//(1)hangi satirdan baslayacagi,tirnak ici yazdirilacak yazi,(3)kac harfli oldugu, (false):siyah ekran uzerine beyaz yazi yazmak icin yazildi.
+  vTaskDelay(3000 / portTICK_PERIOD_MS);//3 saniye bekleme
+
+  ssd1306_display_text(&dev, 3, "esp32", 5, true);//(3)kacinci satirda yazilmaya baslanacak, tirnak ici yazdirilacak yazi, (4)kac harf oldugu, (true):beyaz ekran uzerine siyah yazi yazmak icin yazildi.
+
 }
