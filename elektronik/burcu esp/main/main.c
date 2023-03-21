856a07326cca9941342c3672af8dc91cc6db21bc
BurcuBulut00 <116095710+BurcuBulut00@users.noreply.github.com>
2023-03-21 12:20:56 +0300

    Add files via upload

--- elektronik/burcu esp/main/main.c
+++ elektronik/burcu esp/main/main.c
@@ -4,16 +4,20 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
-
+#include "esp_adc_cal.h"
+#include <sdkconfig.h>
+#include "driver/adc.h"
+#include <driver/gpio.h>
+#include "driver/adc.h"
+#include <time.h>
 #include "ssd1306.h"
 #include "font8x8_basic.h"
 
 //sda ve scl pinlerini tanimlandi.
 #define sda_gpio 22
 #define scl_gpio 23
-
+#define SIG 32
 #define tag "SSD1306"
-
 void app_main(void)
 {
 	SSD1306_t dev; //device tanimlama
@@ -28,10 +32,10 @@ void app_main(void)
 	ssd1306_clear_screen(&dev, true);
 	ssd1306_contrast(&dev, 0xff);//Siyah ve beyaz renklerin daha net gorunmesi icin contrast ayari: 0xff 
 
-    ssd1306_display_text_x3(&dev, center, "HELLO", 5, false);
+    //ssd1306_display_text_x3(&dev, center, "HELLO", 5, false);
     vTaskDelay(3000 / portTICK_PERIOD_MS);
     ssd1306_clear_screen(&dev, false);
-    ssd1306_display_text(&dev, 1, " HIZ:", 5, false);
+    //ssd1306_display_text(&dev, 1, " HIZ:", 5, false);
 
    //Display Count
 	uint8_t image[24];
@@ -52,8 +56,8 @@ void app_main(void)
 	ssd1306_display_image(&dev, top, (6*10), image3, sizeof(image3));    
 	ssd1306_display_image(&dev, top, (6*10), image3, sizeof(image3));
 
-//0'dan 9 a kadar hatali calismiyor
-     for(int font3=0x30; font3<=0x39; font3++)
+
+     /*for(int font3=0x30; font3<=0x39; font3++)
 		{
 		memset(image3, 0, sizeof(image3));
 		ssd1306_display_image(&dev, top-1, (7*6-1), image3, 8);
@@ -88,7 +92,26 @@ void app_main(void)
 
 vTaskDelay(1000 / portTICK_PERIOD_MS);
 ssd1306_clear_screen(&dev, false);
-ssd1306_display_text(&dev, 1, "HIZ:100", 7, false);
+ssd1306_display_text(&dev, 1, "HIZ:100", 7, false);*/
+
+static esp_adc_cal_characteristics_t adc1_chars;
+    
+    adc1_config_width(ADC_WIDTH_BIT_10);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_10, 0, &adc1_chars);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_0);
+
+while (1) 
+    {
+		uint32_t voltage;
+        float adc_value = adc1_get_raw(ADC1_CHANNEL_4);
+	    char degc[20];
+	    sprintf(degc, "%f",adc_value);
+		voltage = esp_adc_cal_raw_to_voltage(adc_value, &adc1_chars);
+  ssd1306_display_text_x3(&dev, 0, degc, 5, false);
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    }
+
 }
 
 
